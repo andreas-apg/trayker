8a8a91fb829d9d41af56a0855666f729d6924260
Andreas <andreasp.gomes@gmail.com>
2019-06-17 04:37:42 +0100

    Base: reinsercao na fila; interface: fila adicionada, melhorias graficas; mesa: funcao read_peso removida

--- esp32/MicroPython/main.py
+++ esp32/MicroPython/main.py
@@ -70,6 +70,10 @@ peso = 0
 #RFID:
 rfid = '[0, 0, 0, 0]'
 def read():
+    sleep(2)
+    hx.tare()
+    print(hx.read())
+    sleep(2)
     while True:
         try:
             global rfid
@@ -78,7 +82,8 @@ def read():
     		    rfid = '[0, 0, 0, 0]'
             global distancia
             distancia = sensor.distance_cm()
-            #peso = hx.read()
+            global peso
+            peso = hx.read()
             msg = (b'{0}|{1}|{2:3}|{3:3}|{4:3}'.format(CLIENT_ID, pronta, distancia, str(rfid),peso))
             #msg = (b'{0}|{1:3}|{2:3}|{3:3}'.format(pronta, distancia, str(rfid), peso))
             client.publish(TOPIC, msg)  # Publish sensor data to MQTT topic
@@ -88,28 +93,7 @@ def read():
             print(er.args[0])
             print('Erro na coleta.')
             continue
-        sleep(1)
-
-def read_peso():
-    sleep(2)
-    hx.tare()
-    print(hx.read())
-    sleep(2)
-    while True:
-        try:
-            global peso
-            peso = hx.read()
-            msg = (b'{0}|{1}|{2:3}|{3:3}|{4:3}'.format(CLIENT_ID, pronta, distancia, str(rfid),peso))
-            #msg = (b'{0}|{1:3}|{2:3}|{3:3}'.format(pronta, distancia, str(rfid), peso))
-            client.publish(TOPIC, msg)  # Publish sensor data to MQTT topic
-            print('Peso.')
-            print(msg)
-
-        except OSError as er:
-            print(er.args[0])
-            print('Erro na coleta do peso.')
-            continue
-        sleep(1)
+        sleep(3)
 
 def waiting():
     global pronta
@@ -136,7 +120,6 @@ def waiting_check():
 
 def working():
      _thread.start_new_thread(read, ())
-     _thread.start_new_thread(read_peso, ())
      _thread.start_new_thread(waiting, ())
 
 working()
